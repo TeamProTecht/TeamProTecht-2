f0c3a8e270327143c6ef954dccd12e7ab0d88721
Fari800k <148209558+Fari800k@users.noreply.github.com>
2023-12-11 09:10:17 +0000

    Add files via upload

---
+++ adminpanel.php
@@ -0,0 +1,65 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!--Specifying character set-->
+        <meta charset="UTF-8"/>
+        <!--Title of web page-->
+        <title>Admin Panel</title>
+        <!--Attaching css file-->
+        <link rel="stylesheet" href="CSS Images\style.css" />
+        <!-- Add icon library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+      <div class="navbar">
+        <!-- Menu Bar -->
+        <ul>
+          <div class="logo-container1">
+            <img src="CSS Images/logo.png" alt="Logo 1">
+          </div>
+            <li><a href="http://localhost/Year%202%20Team%20Project/confirmorders.html">Confirm Orders</a></li>
+            <li><a href="http://localhost/Year%202%20Team%20Project/pendingorders.html">Pending Orders</a></li>
+            <li><a href="http://localhost/Year%202%20Team%20Project/pendingorders.html">View Inventory</a></li>
+            <li><a href="http://localhost/Year%202%20Team%20Project/fulfilledorders.html">Fulfilled Orders</a></li>
+            <li><a href="file:///C:/xampp/htdocs/Year%202%20Team%20Project/stocksystem.html">Stock Put Away</a></li>
+            <li class="logout-link"><a href="http://localhost/Year%202%20Team%20Project/login.html" onclick="logout()">Logout</a></li>
+        </ul>
+    </div>
+
+<div class="main-content">
+    <div class="wrapper">
+        <br>
+        <br>
+        <h1 class="text-center">Manage Admin</h1>
+
+        <br> <br>
+
+        <a href="add-admin.php" class="p-3 mb-2 bg-success text-white">Add Admin</a>
+
+        <br> <br> <br>
+
+        <table>
+            <tr>
+                <th>Admin ID</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+            <td>1.</td>
+            <td>John Smith</td>
+            <td>JohnSmith1</td>
+            <td>
+                <button><a href="#" class="btn-secondary">Update Admin</a></button>
+            </td>
+        </table>
+    </div>
+</div>
+    
+</body>
+</html>
