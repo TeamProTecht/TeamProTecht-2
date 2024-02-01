a05266770a62b48b852bdad66d63a6112dd893a7
HuseinDawood <h.dawood1360@gmail.com>
2024-02-01 11:26:16 +0000

    added the about us page for jai to work on

---
+++ aboutus.php
@@ -0,0 +1,37 @@
+<!--Specifying that we using HTML-->
+<!DOCTYPE html>
+
+<!--Specifying that language is English-->
+<html lang="en">
+
+    <head>
+        <!--Specifying character set-->
+        <meta charset="UTF-8"/>
+        <!--Title of web page-->
+        <title>Teamprotecht</title>
+        <!--Attaching css file-->
+        <link rel="stylesheet" href="aboutus.css" />
+        <!-- Add java script -->
+        <script defer src="JavaScript/script.js"></script>
+        <!-- Add icon library -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <!--Creating the navigation bar-->
+                <nav>
+                    <ul> 
+                        <!--3 different tabs with links to each page-->
+                        <li><img src="CSS HP/images/logo.png" width="90px" height="65px"></li>
+                        <li><a href="Teamprotecht - HomePage.html">Home</a></li>
+                        <li><a href="browse.php">Browse</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li style="float:right"><a href="Product_Basket.php"><i class="fa fa-shopping-basket"></i></a></li>
+                        <li style="float:right"><a href="#"><i class="fa fa-user"></i></a></li>
+                        
+                    </ul>
+                </nav>
+            <br>
+            <br>
+               <h1>About Us</h1>
+               <html>
