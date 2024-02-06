7f5c7d3401008658e4669e1fefcc35efa6623756
georgesmartx01 <copilaugeorgeandrei87@gmail.com>
2024-02-06 21:27:20 +0000

    update

--- userside/browse.php
+++ userside/browse.php
@@ -9,10 +9,10 @@
 <body>
     <nav>
         <ul> 
-            <img src="CSS HP/images/logo.png" width="90px" height="65px">
-            <a href="Teamprotecht - HomePage.html">Home</a>
-            <a href="browse.php">Browse</a>
-            <a href="">About Us</a>
+            <li><img src="CSS HP/images/logo.png" width="90px" height="65px"></li>
+            <li><a href="Teamprotecht - HomePage.html">Home</a></li>
+            <li><a href="browse.php">Browse</a></li>
+            <li><a href="">About Us</a></li>
             <li style="float:right"><a href="Product_Basket.php"><i class="fa fa-shopping-basket"></i></a></li>
             <li style="float:right"><a href="#"><i class="fa fa-user"></i></a></li>
         </ul>
