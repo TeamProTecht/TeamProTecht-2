4d7c08126def550bd3baf63db84810fe203eb90a
HuseinDawood <h.dawood1360@gmail.com>
2024-02-20 15:01:29 +0000

    remove all files- copied main

--- userside/SQL/connectdb.php
+++
@@ -1,9 +0,0 @@
-//this allows the user to connect to the database, please use this when connecting within your php  
-<?php
-
-$con=mysqli_connect('localhost','root','','cs2tp');
-if(!$con){
-    die(mysqli_error($con));
-}
-
-?>
