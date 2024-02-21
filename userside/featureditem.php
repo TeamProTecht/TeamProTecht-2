b4504d9e3011bae8743a10605c5c6be9b3ddd953
Fari800k <148209558+Fari800k@users.noreply.github.com>
2024-02-21 09:54:41 +0000

    Add files via upload

---
+++ userside/featureditem.php
@@ -0,0 +1,42 @@
+<?php
+// Connect to Database 
+include 'connectdb.php';
+
+?>
+<?php
+//Homepage 
+include 'Teamprotecht - HomePage.html'
+
+?>
+
+<?php
+// SQL query to retrieve 4 items with the lowest quantity (greater than or equal to 1)
+$query = "SELECT * FROM item WHERE Quantity >= 1 ORDER BY Quantity ASC LIMIT 4";
+$result = $conn->query($query);
+
+// Check if there are results
+if ($result->num_rows > 0) {
+  
+  // Outer Container 
+  echo '<div style="display: flex; flex-wrap: wrap;">';
+    // Output of each row 
+    while ($row = $result->fetch_assoc()) {
+        // Display of each item
+        echo'<div style="border: 1px solid #000; padding: 10px; margin: 5px;">';
+        echo "Best Seller: " . $row["ItemName"]  . ", Price: " . $row["Price"];
+        echo '</div><br>';   
+        echo '<button onclick="addToBasket(' . $row["Item_ID"] . ')">Add to Basket</button>';
+        echo '<button onclick="viewMore(' . $row["Item_ID"] . ')">View More</button>';
+
+    }
+    // Close the container
+ echo '</div>';
+
+} else {
+    echo "Featured Products to be announced";
+}
+?>
+
+
+  
+               
