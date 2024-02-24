a4f5fdc6a75ad420a0e7a15e5a085122230cd6b4
DanAlexCox <149510425+DanAlexCox@users.noreply.github.com>
2024-02-24 20:39:07 +0000

    Add files via upload
    contains all content in the customer website footer

---
+++ userside/footer.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="CSS/footer.css">
+
+<footer>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h4>Contact Us</h4>
+            <p>Email: group3.cs2tp@gmail.com</p>
+            <p>Phone: +44 (0) 123456789</p>
+          </div>
+          <div class="col-md-6">
+            <h4>Follow Us</h4>
+            <p>Connect with us on social media:</p>
+            <ul class="social-icons">
+              <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook" width="30" height="30"></a></li>
+              <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter" width="30" height="30"></a></li>
+              <li><a href="https://www.instagram.com/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram" width="30" height="30"></a></li>
+            </ul>
+          </div>
+        </div>
+    </div>
+    <div class="text-center">
+        <p>&copy; All rights reserved. </p>
+    </div>
+</footer>
