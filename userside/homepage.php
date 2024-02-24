737408953cafc73b02027edc365844ccd3f791f4
DanAlexCox <149510425+DanAlexCox@users.noreply.github.com>
2024-02-24 20:37:09 +0000

    Update homepage.php
    Put footer info content into footer.php

--- userside/homepage.php
+++ userside/homepage.php
@@ -62,31 +62,8 @@ include "navbar.php";
       <h1>Best Sellers</h1>                        
     </div>      
     </main>
-
-
-    <footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <h4>Contact Us</h4>
-        <p>Email: group3.cs2tp@gmail.com</p>
-        <p>Phone: +44 (0) 123456789</p>
-      </div>
-      <div class="col-md-6">
-        <h4>Follow Us</h4>
-        <p>Connect with us on social media:</p>
-        <ul class="social-icons">
-          <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="facebook.png" alt="Facebook" width="30" height="30"></a></li>
-          <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="twitter.png" alt="Twitter" width="30" height="30"></a></li>
-          <li><a href="https://www.instagram.com/" target="_blank"><img src="insta.png" alt="Instagram" width="30" height="30"></a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="text-center">
-    <p>&copy; All rights reserved. </p>
-  </div>
-</footer>
+<!-- Add footer -->
+<?php include "footer.php";?>
 
   </body>
 </html>
