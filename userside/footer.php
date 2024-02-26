47b8c7a0caa952a79152b0a66b979c3ed61c9cbf
mmh24 <149673566+mmh24@users.noreply.github.com>
2024-02-26 21:40:15 +0000

    footer update

--- userside/footer.php
+++ userside/footer.php
@@ -1,25 +1,22 @@
 <link rel="stylesheet" href="CSS/footer.css">
 
 <footer>
-    <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <h4>Contact Us</h4>
-            <p>Email: group3.cs2tp@gmail.com</p>
-            <p>Phone: +44 (0) 123456789</p>
-          </div>
-          <div class="col-md-6">
-            <h4>Follow Us</h4>
-            <p>Connect with us on social media:</p>
-            <ul class="social-icons">
-              <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook" width="30" height="30"></a></li>
-              <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter" width="30" height="30"></a></li>
-              <li><a href="https://www.instagram.com/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram" width="30" height="30"></a></li>
-            </ul>
-          </div>
+        <div class="footer-content">
+            <div class="footer-section">
+                <h3>Social Media</h3>
+                <ul class="social-icons">
+                    <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="fb.png" alt="Facebook"></a></li>
+                    <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="tw.png" alt="Twitter"></a></li>
+                    <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="ig.png" alt="Instagram"></a></li>
+                </ul>
+            </div>
+            
+            <div class="footer-section">
+                <h3>Contact</h3>
+                <p>Email: group3.cs2tp@gmail.com</p>
+                <p>Phone: +44 (0) 123456789</p>
+            </div>
         </div>
-    </div>
-    <div class="text-center">
-        <p>&copy; All rights reserved. </p>
-    </div>
-</footer>
+        
+        <p class="copyright">&copy; 2024 Phone Store. All rights reserved.</p>
+    </footer>
