a09160e46856936c36cd24bfbbb032a916d43f83
DanAlexCox <149510425+DanAlexCox@users.noreply.github.com>
2024-02-27 10:58:27 +0000

    Adjustments to class names and image locations

--- userside/footer.php
+++ userside/footer.php
@@ -1,17 +1,17 @@
 <link rel="stylesheet" href="CSS/footer.css">
 
 <footer>
-        <div class="footer-content">
-            <div class="footer-section">
+        <div class="footer_content">
+            <div class="social_media">
                 <h3>Social Media</h3>
                 <ul class="social-icons">
-                    <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="fb.png" alt="Facebook"></a></li>
-                    <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="tw.png" alt="Twitter"></a></li>
-                    <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="ig.png" alt="Instagram"></a></li>
+                    <li><a href="https://en-gb.facebook.com/" target="_blank"><img src="CSS/images/facebook.png" alt="Facebook" width="50px" height="50px"></a></li>
+                    <li><a href="https://twitter.com/i/flow/login" target="_blank"><img src="CSS/images/twitter.png" alt="Twitter" width="50px" height="50px"></a></li>
+                    <li><a href="https://www.instagram.com/accounts/login/" target="_blank"><img src="CSS/images/insta.png" alt="Instagram" width="50px" height="50px"></a></li>
                 </ul>
             </div>
             
-            <div class="footer-section">
+            <div class="contact_info">
                 <h3>Contact</h3>
                 <p>Email: group3.cs2tp@gmail.com</p>
                 <p>Phone: +44 (0) 123456789</p>
